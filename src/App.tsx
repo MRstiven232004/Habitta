6a3b56bb148d9213aa209a73cf7194666439bb59
MRstiven232004 <stiven20042316@gmail.com>
2025-11-25 07:01:49 -0500

    creacion de upper bar
    eliminacion del index.css y creacion del upper bar

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,33 @@ function App() {
     <>
       <header>
         <div className="logo-container">
-          <img className="logo" src="/src/assets/logoSF.png" alt="no image" />
+          <a href="">
+            <img className="logo" src="/src/assets/logoSF.png" alt="no image" />
+          </a>
         </div>
-        <div className="nav">
-          <a href="" className="navLinks"></a>
-          <a href="" className="navLinks"></a>
-          <a href="" className="navLinks"></a>
-        </div>
+        <nav>
+          <a href="" className="navLinks">
+            Inicio
+          </a>
+          <a href="" className="navLinks">
+            Propiedades
+          </a>
+          <a href="" className="navLinks">
+            Herramientas
+          </a>
+          <a href="" className="navLinks">
+            Favoritos
+          </a>
+          <a href="" className="navLinks">
+            Mi panel
+          </a>
+          <a href="" className="navLinks">
+            +Publicar
+          </a>
+        </nav>
+        <a href="">
+          <img src="/src/icons/avatar.png" alt="" />
+        </a>
       </header>
     </>
   );
